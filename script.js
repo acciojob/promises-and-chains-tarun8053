883d54b1a27a15f9f9e1114bd132ecff7ac77ad1
Tarun <thakurtarun1012@gmail.com>
2025-11-25 06:12:47 -0800

    created a submission

--- script.js
+++ script.js
@@ -3,27 +3,35 @@ const age = document.getElementById("age");
 const nameField = document.getElementById("name");
 const btn = document.getElementById("btn");
 
-const promise = (ageVal) => {
-    return new Promise((res, rej) => {
-        if (ageVal > 18) {
-            res("You can vote.");
-        } else {
-            rej("Oh sorry. You aren't old enough.");
-        }
+function checkAge(ageVal, userName) {
+    return new Promise((resolve, reject) => {
+
+        setTimeout(() => {
+
+            if (ageVal > 18) {
+                resolve(`Welcome, ${userName}. You can vote.`);
+            } else {
+                reject(`Oh sorry ${userName}. You aren't old enough.`);
+            }
+
+        }, 4000); // 4-second delay
+
     });
-};
+}
 
 function run() {
 
-    // validate inside click, not outside
     if (age.value === "" || nameField.value === "") {
         alert("Please fill all fields.");
         return;
     }
 
-    promise(Number(age.value))
-        .then(msg => alert(msg))
-        .catch(err => alert(err));
+    const userAge = Number(age.value);
+    const userName = nameField.value;
+
+    checkAge(userAge, userName)
+        .then(message => alert(message))
+        .catch(error => alert(error));
 }
 
 btn.addEventListener("click", run);
